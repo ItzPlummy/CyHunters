b0196308ebfd57721cfe65b6da8bacebbd5b6591
Plummy <plummybeatsoff@gmail.com>
2025-06-09 01:17:47 +0200

    Initial commit

---
+++ src/main/java/com/plummy/cyhunters/CyHunters.java
@@ -0,0 +1,17 @@
+package com.plummy.cyhunters;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CyHunters extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

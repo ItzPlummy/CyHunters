4e967c555741406411c8ac4b85aaf31f0fb551dd
Plummy <plummybeatsoff@gmail.com>
2025-06-09 01:27:29 +0200

    Add instance and logger

--- src/main/java/com/plummy/cyhunters/CyHunters.java
+++ src/main/java/com/plummy/cyhunters/CyHunters.java
@@ -2,16 +2,37 @@ package com.plummy.cyhunters;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class CyHunters extends JavaPlugin {
+    private static CyHunters instance;
+    private static Logger logger;
+
+    {
+        logger = getLogger();
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        logger().info("Enabling CyHunters...");
+
+        logger().info("CyHunters Enabled!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        logger().info("Disabling CyHunters...");
+
+        logger().info("CyHunters Disabled!");
+    }
+
+    public CyHunters getInstance() {
+        return instance;
+    }
+
+    public Logger logger() {
+        return logger;
     }
 }
